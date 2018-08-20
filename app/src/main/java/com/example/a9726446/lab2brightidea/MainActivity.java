c99540f4c15f234f3b4ac84128ded69674e9a920
swin9726446 <9726446@student.swin.edu.au>
2018-08-20 20:47:39 +1000

    Pt.5: LongPress Commit WIP @ LB1-MAC-024 / Marked out where code needs to be changed.

--- app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
+++ app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a9726446.lab2brightidea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isLightOn = false;
 
+
     private View.OnTouchListener touchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+            // Tasks 2 - 4
             if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
                 toggleLight();
                 v.performClick();
