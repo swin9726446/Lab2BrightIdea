1b0a9e1d4c58b10c2d94c869d9eaad7bfb8d7645
swin9726446 <9726446@student.swin.edu.au>
2018-08-22 16:35:47 +1000

    Task 5: Long Touch @ LB1-MAC-024 + Gesture Detector to handle long touch events. / Tried to get it to work with a 'modern' constructor but lost too much time with resulting errors.

--- app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
+++ app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.a9726446.lab2brightidea;
 
+//import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AndroidException;
+//import android.util.AndroidException;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -14,17 +15,35 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isLightOn = false;
 
-
     private View.OnTouchListener touchListener = new View.OnTouchListener() {
+
+        // Task 5
+        // This is just a lab exercise, I'm okay with using a depreciated method for now.
+        // GestureDetector gDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener()){
+        GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener(){
+            public void onLongPress(MotionEvent e){
+                    toggleLight();
+            }
+        });
+
+        /*private Context getContext() {
+            try{
+                return getApplicationContext();
+            } catch (Exception e){
+                Log.d("initUI", "Error producing Application Context:\n" + e.toString());
+                return null;
+            }
+        }*/
+
         @Override
         public boolean onTouch(View v, MotionEvent event) {
             // Tasks 2 - 4
-            if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
-                toggleLight();
-                v.performClick();
-                //v.setOnTouchListener(null);
-                Log.d("Listener", "TAP");
-            }
+            // if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+            //    toggleLight();
+            //    Log.d("Listener", "TAP");
+            // }
+            gestureDetector.onTouchEvent(event);
+            v.performClick();
             return true;
         }
     };
