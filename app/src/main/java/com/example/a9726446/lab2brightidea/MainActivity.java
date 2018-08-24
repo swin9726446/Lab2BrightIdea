8686311c4d51e0f62e7a601828fbf6ad350b1051
swin9726446 <9726446@student.swin.edu.au>
2018-08-24 13:30:02 +1000

    Feedback from tutor @ EN302-018 - Replaced depreciated GestureDetector constructor with one using current context / Reaffirmed means of checking correct image. TODO: Update modifed GD so that it works again.

--- app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
+++ app/src/main/java/com/example/a9726446/lab2brightidea/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.a9726446.lab2brightidea;
 
-//import android.content.Context;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.util.AndroidException;
+import android.util.AndroidException;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -15,25 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isLightOn = false;
 
-    private View.OnTouchListener touchListener = new View.OnTouchListener() {
+    //Isuru the sage, suggested make this it's own stored variable doodad.
+    // This way it can be used several times.
+    // ALSO! Context! There's applciation, view and etc etc context, so you should just have to pass the thing (see initUI())
+    private View.OnTouchListener touchListener(final Context context){
+      return new View.OnTouchListener() {
 
         // Task 5
         // This is just a lab exercise, I'm okay with using a depreciated method for now.
-        // GestureDetector gDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener()){
-        GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener(){
-            public void onLongPress(MotionEvent e){
+
+         GestureDetector gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener()){
+        // GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener(){
+
+            //TODO: Fix this following suggestions from Isuru.
+             public void onLongPress(MotionEvent e){
                     toggleLight();
             }
-        });
-
-        /*private Context getContext() {
-            try{
-                return getApplicationContext();
-            } catch (Exception e){
-                Log.d("initUI", "Error producing Application Context:\n" + e.toString());
-                return null;
-            }
-        }*/
+        };
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
@@ -46,12 +44,14 @@ public class MainActivity extends AppCompatActivity {
             v.performClick();
             return true;
         }
-    };
+      };
+    }
 
     private void toggleLight() {
         ImageView ivLight = findViewById(R.id.imgLight);
         //Comparing drawables doesn't work. Let's just take the cheat's way and record the state separately. :/
 
+        //And comparing drawables is virtually impossible. You actually do have to use an associated tag ahahaha.
         /*Drawable dLight = ivLight.getDrawable();
         Drawable dLightOn = getDrawable(R.drawable.lightbulb_on);
         Drawable dLightOff = getDrawable(R.drawable.lightbulb_off);*/
@@ -87,8 +87,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initaliseUI(Bundle b) {
-        findViewById(R.id.imgLight).setOnTouchListener(touchListener);
-        findViewById(R.id.txtLight).setOnTouchListener(touchListener);
+        //"This" is also a context! And it's what we're using so why not pass it along!
+        findViewById(R.id.imgLight).setOnTouchListener(touchListener(this));
+        findViewById(R.id.txtLight).setOnTouchListener(touchListener(this));
         if (b == null) return; //move along, nothing to see here.
 
         //Flip the value, so you can then 'toggle' it back to what it should be! >_>
